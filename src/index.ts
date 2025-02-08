0e301865cb94ecb7954e38701adb90e95821bb2d
Vincent Duchêne <vincentduchene.m@gmail.com>
2025-02-09 00:04:45 +0100

    feat: first mcp server

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
-import { sum } from '@/sum/sum';
+import { McpServer } from '@modelcontextprotocol/sdk/server/mcp.js';
+import { StdioServerTransport } from '@modelcontextprotocol/sdk/server/stdio.js';
+import { z } from 'zod';
 
-const result = sum(1, 2);
+const server = new McpServer({
+    name: 'Demo sum',
+    version: '1.0.0',
+});
 
-console.log(`The result of sum(1, 2) is ${result}`);
+server.tool('add', { a: z.number(), b: z.number() }, async ({ a, b }) => ({
+    content: [{ text: String(a + b), type: 'text' }],
+}));
+
+const transport = new StdioServerTransport();
+await server.connect(transport);
